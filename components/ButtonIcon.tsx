68991e015918fdd67a53b346ea3e89b472fe3271
Bilal <bilalbi1992@gmail.com>
2024-04-03 15:15:34 +0700

    feat: add ButtonIcon

---
+++ components/ButtonIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import style from "./button-icon.module.scss";
+import clsx from "clsx";
+import ButtonBase from "./ButtonBase";
+import type { Classes } from "./ButtonBase";
+
+type ButtonProps = Omit<React.ComponentProps<typeof ButtonBase>, "classes"> & {
+    classes?: Partial<Classes>;
+};
+
+export default function Button({ classes: externalClasses, ...props }: ButtonProps) {
+    const defaultClasses = {
+        root: style.button,
+        variant: {
+            solid: style.solid,
+            outlined: style.outlined, 
+        },
+        color: {
+            primary: style.primary,
+            secondary: style.secondary,
+        },
+        size: {
+            sm: style.sm,
+            md: style.md,
+            lg: style.lg
+        }
+    };
+
+    const classes = {
+        ...defaultClasses,
+        ...externalClasses,
+    };
+
+    return (
+        <ButtonBase 
+            classes={classes}
+            {...props} 
+        />
+    );
+}
