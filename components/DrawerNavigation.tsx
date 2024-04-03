0dee7a22868c8b1eec4b8f5a02e04789c8066627
Bilal <bilalbi1992@gmail.com>
2024-04-03 16:14:06 +0700

    refactor: tweak

--- components/DrawerNavigation.tsx
+++ components/DrawerNavigation.tsx
@@ -13,6 +13,8 @@ import {
 import { usePathname } from "next/navigation";
 import Logo from "./Logo";
 import Navigation from "./Navigation";
+import Close from "../icons/Close";
+import List from "../icons/List";
 
 export default function DialogNavigation() {
     const [open, setOpen] = React.useState(false);
@@ -43,17 +45,13 @@ export default function DialogNavigation() {
         <Dialog open={open} onOpenChange={setOpen}>
             <nav aria-label="Dialog Navigation" className={style.nav}>
                 <DialogTrigger className={style.trigger}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                    </svg>
+                    <List />
                 </DialogTrigger>
                 <DialogContent overlay={style.overlay} className={style.dialog}>
                     <header className={style.header}>
                         <Logo className={style.logo} />
                         <DialogClose className={style.close}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" viewBox="0 0 16 16">
-                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                            </svg>
+                            <Close />
                         </DialogClose>
                     </header>
                     <div>
