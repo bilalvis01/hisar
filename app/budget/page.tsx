ab508a8d9153fb71b8b88312d49d447d4835f047
Bilal <bilalbi1992@gmail.com>
2024-04-03 16:03:15 +0700

    refactor: use Add button instead raw button

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import style from "./budget.module.scss";
 import Table from "./Table";
+import Add from "./Add";
 
 export default function Page() {
     return (
         <div className={style.card}>
             <header className={style.header}>
-                <button>
-                    Tambah
-                </button>
+                <Add heading="Tambah Budget" />
             </header>
             <Table />
         </div>
