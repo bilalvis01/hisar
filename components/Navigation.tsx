b98115f3c1539527e6d7aa36998f587b7c144923
Bilal <bilalbi1992@gmail.com>
2024-04-07 23:18:31 +0700

    feat: add component navigation tab primary

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -15,15 +15,39 @@ export default function Navigation({
     classes 
 }: NavigationProps) {
     return (
-        <ul className={clsx(style.root, classes.root)} role={role} aria-label={ariaLabel}>
-            <li role="none">
-                <Link role="menuitem" className={classes.menuItem} href="/">Home</Link>
+        <ul className={clsx(style.navigation, classes.root)} role={role} aria-label={ariaLabel}>
+            <li role="none" className={clsx("navigation-tab", "primary", "inline", "active", style.tabWidth)}>
+                <Link role="menuitem" className="container" href="/">
+                    <div className="state-layer">
+                        <div className="content">
+                            <span className="label">Home</span>
+                            <hr />
+                        </div>
+                    </div>
+                    <hr />
+                </Link>
             </li>
-            <li role="none">
-                <Link role="menuitem" className={classes.menuItem} href="/budget">Budget</Link>
+            <li role="none" className={clsx("navigation-tab", "primary", "inline", style.tabWidth)}>
+                <Link role="menuitem" className="container" href="/budget">
+                    <div className="state-layer">
+                        <div className="content">
+                            <span className="label">Budget</span>
+                            <hr />
+                        </div>
+                    </div>
+                    <hr />
+                </Link>
             </li>
-            <li role="none">
-                <Link role="menuitem" className={classes.menuItem} href="/expanse">Expanse</Link>
+            <li role="none" className={clsx("navigation-tab", "primary", "inline", style.tabWidth)}>
+                <Link role="menuitem" className="container" href="/expense">
+                    <div className="state-layer">
+                        <div className="content">
+                            <span className="label">Expense</span>
+                            <hr />
+                        </div>
+                    </div>
+                    <hr />
+                </Link>
             </li>
         </ul>
     );
