2c6f856c3a09d7df0769f0a0550136499a459291
Bilal <bilalbi1992@gmail.com>
2024-04-07 13:55:12 +0700

    feat: add icon button filled

---
+++ components/IconButtonFilled.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import IconButtonBase from "./IconButtonBase";
+
+export default function IconButtonFilled(props) {
+    return <IconButtonBase variant="filled" {...props} />;
+}
