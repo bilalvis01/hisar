993ba996be8ce69e9390d338acb7ef703d1f0e1c
Bilal <bilalbi1992@gmail.com>
2024-04-11 21:35:33 +0700

    refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
 import React from "react";
 import style from "./home.module.scss";
+import CardFilled from "../components/CardFilled";
+import clsx from "clsx";
+import format from "../utils/format";
 
 export default function Page() {
     return (
-        <div className={style.wrapper}>
-            <div className={style.card}>
-                <h3 className={style.cardHeader}>TOTAL BUDGET</h3>
-                <p className={style.cardContent}>Rp. 500.000,-</p>
-            </div>
-            <div className={style.card}>
-                <h3 className={style.cardHeader}>TOTAL EXPANSE</h3>
-                <p className={style.cardContent}>Rp. 300.000,-</p>
-            </div>
-            <div className={style.card}>
-                <h3 className={style.cardHeader}>SISA</h3>
-                <p className={style.cardContent}>Rp. 200.000,-</p>
-            </div>
+        <div className={style.cardContainer}>
+            <CardFilled className={style.card}>
+                <div className={style.cardContent}>
+                    <h2 className={clsx(style.cardHeader, "text-title-medium")}>TOTAL BUDGET</h2>
+                    <p className={clsx(style.cardBody, "text-headline-small")}>{format.currency(500_000)}</p>
+                </div>
+            </CardFilled>
+            <CardFilled className={style.card}>
+                <div className={style.cardContent}>
+                    <h2 className={clsx(style.cardHeader, "text-title-medium")}>TOTAL EXPANSE</h2>
+                    <p className={clsx(style.cardBody, "text-headline-small")}>{format.currency(300_000)}</p>
+                </div>
+            </CardFilled>
+            <CardFilled className={style.card}>
+                <div className={style.cardContent}>
+                    <h2 className={clsx(style.cardHeader, "text-title-medium")}>SISA</h2>
+                    <p className={clsx(style.cardBody, "text-headline-small")}>{format.currency(200_000)}</p>
+                </div>
+            </CardFilled> 
         </div>
     )
 }
