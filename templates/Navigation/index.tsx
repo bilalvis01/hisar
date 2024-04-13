7cd219681432cefd415b5336d16bbe263ce717df
Bilal <bilalbi1992@gmail.com>
2024-04-13 21:45:56 +0700

    wrap TabLinks with TabBar

--- templates/Navigation/index.tsx
+++ templates/Navigation/index.tsx
@@ -1,28 +1,32 @@
 import React from "react";
-import clsx from "clsx";
 import style from "./navigation.module.scss";
 import TabLink from "../../components/TabLink";
+import TabBar from "../../components/TabBar";
 
 interface NavigationProps {
     className?: string;
+    select?: string;
 }
 
 export default function Navigation({ 
     className,
+    select,
 }: NavigationProps) {
     return (
         <nav className={className} aria-label="Main Navigation">
-            <ul className={style.list} role="navigation" aria-label="Main Navigation">
-                <li role="none">
-                    <TabLink href="/" className={style.tabWidth}>Home</TabLink>
-                </li>
-                <li role="none">
-                    <TabLink href="/budget" className={style.tabWidth}>Budget</TabLink>
-                </li>
-                <li role="none">
-                    <TabLink href="/expense" className={style.tabWidth}>Expense</TabLink>
-                </li>
-            </ul>
+            <TabBar select={select}>
+                <ul className={style.list} role="navigation" aria-label="Main Navigation">
+                    <li role="none">
+                        <TabLink href="/" className={style.tabWidth}>Home</TabLink>
+                    </li>
+                    <li role="none">
+                        <TabLink href="/budget" className={style.tabWidth}>Budget</TabLink>
+                    </li>
+                    <li role="none">
+                        <TabLink href="/expense" className={style.tabWidth}>Expense</TabLink>
+                    </li>
+                </ul>
+            </TabBar>
         </nav>
     );
 }
