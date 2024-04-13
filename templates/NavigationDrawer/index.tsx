0ed8b8585b3b37b3b209d996adb693884d1bd8a3
Bilal <bilalbi1992@gmail.com>
2024-04-13 19:52:34 +0700

    refactor template component NavigationDrawer

--- templates/NavigationDrawer/index.tsx
+++ templates/NavigationDrawer/index.tsx
@@ -1,34 +1,81 @@
+"use client"
+
 import React from "react";
 import { 
     NavigationDrawer as NavigationDrawerBase, 
-    Link as NavigationDrawerLink, 
-    Header as NavigationDrawerHeader,
-    Close as NavigationDrawerClose, 
+    NavigationDrawerContent,
+    NavigationDrawerBody,
+    NavigationLink, 
+    NavigationHeader, 
+    NavigationHeadline,
 } from "../../components/NavigationDrawer";
-import Logo from "../../templates/Logo";
-import style from "./navigation-drawer.module.scss";
+import { usePathname } from "next/navigation";
+import IconButtonFilled from "../../components/IconButtonFilled";
+import IconButtonStandard from "../../components/IconButtonStandard";
+import IconClose from "../../icons/Close";
+import IconList from "../../icons/List";
 
 interface NavigationDrawerProps {
     className?: string,
 }
 
 export default function NavigationDrawer({ className }: NavigationDrawerProps) {
+    const [open, setOpen] = React.useState(false);
+    const pathname = usePathname()
+    const [prevPathname, setPrevPathname] = React.useState(pathname);
+
+    const handleResize = React.useCallback(() => {
+        if (window.innerWidth >= 992) {
+            setOpen(false);
+        }
+    }, [setOpen]);
+
+    React.useEffect(() => {
+        window.addEventListener("resize", handleResize);
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, [handleResize]);
+
+    React.useEffect(() => {
+        if (prevPathname != pathname) {
+            setPrevPathname(pathname);
+            setOpen(false);
+        }
+    }, [setOpen, setPrevPathname, pathname, prevPathname]);
+
     return (
-        <NavigationDrawerBase className={className}>
-            <NavigationDrawerHeader className={style.header}>
-                <NavigationDrawerClose />
-            </NavigationDrawerHeader>
-            <ul>
-                <li>
-                    <NavigationDrawerLink href={"/"}>Home</NavigationDrawerLink>
-                </li>
-                <li>
-                    <NavigationDrawerLink href={"/budget"}>Budget</NavigationDrawerLink>
-                </li>
-                <li>
-                    <NavigationDrawerLink href={"/expense"}>Expense</NavigationDrawerLink>
-                </li>
-            </ul>
-        </NavigationDrawerBase>
+        <nav className={className}>
+            <NavigationDrawerBase 
+                variant="modal"
+                open={open}
+                onOpenChange={setOpen}
+                select={pathname}
+            >
+                <IconButtonFilled onClick={() => setOpen(true)}>
+                    <IconList />
+                </IconButtonFilled>
+                <NavigationDrawerContent>
+                    <NavigationHeader>
+                        <NavigationHeadline>HISAR</NavigationHeadline>
+                        <IconButtonStandard onClick={() => setOpen(false)}>
+                            <IconClose />
+                        </IconButtonStandard>
+                    </NavigationHeader>
+                    <NavigationDrawerBody>
+                        <ul>
+                            <li>
+                                <NavigationLink href={"/"}>Home</NavigationLink>
+                            </li>
+                            <li>
+                                <NavigationLink href={"/budget"}>Budget</NavigationLink>
+                            </li>
+                            <li>
+                                <NavigationLink href={"/expense"}>Expense</NavigationLink>
+                            </li>
+                        </ul>
+                    </NavigationDrawerBody>
+                </NavigationDrawerContent>
+            </NavigationDrawerBase>
+        </nav>
     );
 }
