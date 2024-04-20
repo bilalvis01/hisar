9671cbbc833b078cdda68320f9fe3d0a61438b93
Bilal <bilalbi1992@gmail.com>
2024-04-20 17:56:23 +0700

    refactor page budget

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import style from "./budget.module.scss";
 import Table from "../../templates/budget-table/BudgetTable";
-import BudgetAddFormMobile from "../../templates/budget-add-from-mobile/BudgetAddFormMobile";
-import BudgetAddFormDesktop from "../../templates/BudgetAddFormDesktop";
+import BudgetAddForm from "../../templates/budget-add-form/BudgetAddForm";
+import clsx from "clsx";
 
 export default function Page() {
     return (
@@ -10,14 +10,16 @@ export default function Page() {
             <div className={style.container}>
                 <div className={style.card}>
                     <header className={style.header}>
-                        <BudgetAddFormDesktop />
+                        <h2 className={clsx("text-title-medium", style.headline)}>Tambah Budget</h2>
+                        <div className={style.toolbar}>
+                            <BudgetAddForm />
+                        </div>
                     </header>
                     <div className={style.body}>
                         <Table />
                     </div>
                 </div>
             </div>
-            <BudgetAddFormMobile />
         </>
     );
 }
