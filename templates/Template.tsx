a7ca610792ddb99cb940cdbd165825d5a379dceb
Bilal <bilalbi1992@gmail.com>
2024-04-22 20:28:43 +0700

    connect apollo client to react

--- templates/Template.tsx
+++ templates/Template.tsx
@@ -1,6 +1,19 @@
+"use client";
+
 import React from "react";
 import style from "./Template.module.scss";
 import Header from "./header/Header";
+import { 
+    ApolloClient, 
+    InMemoryCache, 
+    ApolloProvider, 
+    gql 
+} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: "/api/graphql",
+    cache: new InMemoryCache()
+});
 
 export default function Template({
     children,
@@ -15,15 +28,17 @@ export default function Template({
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
             </head>
             <body>
-                <div className={style.app}>
-                    <main className={style.main}>
-                        {children}
-                    </main>
-                    <footer className={style.footer}>
-                        2024
-                    </footer>
-                    <Header className={style.header} />
-                </div>
+                <ApolloProvider client={client}>
+                    <div className={style.app}>
+                        <main className={style.main}>
+                            {children}
+                        </main>
+                        <footer className={style.footer}>
+                            2024
+                        </footer>
+                        <Header className={style.header} />
+                    </div>
+                </ApolloProvider>
             </body>
         </html>
     )
