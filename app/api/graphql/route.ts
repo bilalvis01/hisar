b6a9db019d4a108fd20c4e49c1443d2079e224e8
Bilal <bilalbi1992@gmail.com>
2024-04-23 21:28:58 +0700

    add mutation createBudget resolvers

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,12 +3,13 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { Resolvers } from "../../../lib/resolvers-types";
 import { readFileSync } from 'fs';
 import { PrismaClient } from "@prisma/client";
+import { createBudget } from "../../../lib/transaction";
 
 const resolvers: Resolvers = {
     Query: {
         async budgets(_, __, context) {
             const data = await context.dataSources.account.findMany({ 
-                where: { type: "budget" },
+                where: { code: 101 },
                 include: {
                     entries: true,
                 }
@@ -35,7 +36,7 @@ const resolvers: Resolvers = {
 
         async expenses(_, __, context) {
             const data = await context.dataSources.ledger.findMany({ 
-                where: { entries: { some: { account: { type: "expense" } } } },
+                where: { entries: { some: { account: { code: 200 } } } },
                 include: {
                     entries: {
                         include: {
@@ -46,8 +47,8 @@ const resolvers: Resolvers = {
             });
         
             return data.map((record) => {
-                const budgetAccount = record.entries.filter(entry => entry.account.type == "budget")[0].account;
-                const expenseEntry = record.entries.filter(entry => entry.account.type == "expense")[0];
+                const budgetAccount = record.entries.filter(entry => entry.account.code === 101)[0].account;
+                const expenseEntry = record.entries.filter(entry => entry.account.code === 200)[0];
                 return {
                     id: record.id,
                     description: record.description,
@@ -58,6 +59,39 @@ const resolvers: Resolvers = {
                     updatedAt: record.updatedAt.toISOString(),
                 };
             });
+        }
+    },
+
+    Mutation: {
+        async createBudget(_, { input }, context) {
+            try {
+                const account = await createBudget(
+                    context.dataSources, 
+                    input.name,
+                    BigInt(input.budget) * BigInt(10000),
+                );
+                return {
+                    code: 200,
+                    success: true,
+                    message: `akun ${input.name} berhasil dibuat`,
+                    budget: {
+                        id: account.id,
+                        name: account.name,
+                        budget: Number(account.balance),
+                        expense: 0,
+                        balance: Number(account.balance),
+                        createdAt: account.createdAt.toISOString(),
+                        updatedAt: account.updatedAt.toISOString(),
+                    },
+                };
+            } catch (err) {
+                return {
+                    code: 500,
+                    success: false,
+                    message: `akun ${input.name} gagal dibuat`,
+                    budget: null,
+                }
+            }
         }
     }
 }
