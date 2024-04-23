e72bdd6b64f0d0f26e19bfb355934b2e1565dbf7
Bilal <bilalbi1992@gmail.com>
2024-04-24 00:50:07 +0700

    refactor seeder script

--- seed.dev.ts
+++ seed.dev.ts
@@ -1,69 +1,69 @@
 import { PrismaClient } from '@prisma/client';
-import { entry } from "./lib/transaction";
+import { entry, createBudget } from "./lib/transaction";
 
 const prisma = new PrismaClient();
 
 const CASH_ACCOUNT_CODE = 100;
 const EXPENSE_ACCOUNT_CODE = 200;
 const BUDGET_ACCOUNT_CODE = 101;
-const CASH_ACCOUNT_ID = 1;
-const EXPENSE_ACCOUNT_ID = 2;
-const BUDGET_PERALATAN_ID = 3;
-const BUDGET_MAKAN_ID = 4;
-const BUDGET_TRANSPORTASI_ID = 5;
 
 async function main() {
+    await prisma.accountCode.createMany({
+        data: [
+            {
+                code: CASH_ACCOUNT_CODE,
+                category: "asset",
+            },
+            {
+                code: EXPENSE_ACCOUNT_CODE,
+                category: "expense",
+            },
+            {
+                code: BUDGET_ACCOUNT_CODE,
+                category: "budget",
+            }
+        ]
+    });
+    const cashAccountCode = await prisma.accountCode.findFirst({
+        where: {
+            code: CASH_ACCOUNT_CODE,
+        }
+    });
+    const expenseAccountCode = await prisma.accountCode.findFirst({
+        where: {
+            code: EXPENSE_ACCOUNT_CODE,
+        }
+    });
     await prisma.account.createMany({
         data: [
             {
-                id: CASH_ACCOUNT_ID,
-                code: CASH_ACCOUNT_CODE,
-                name: "cash",
-                type: "asset",
+                codeId: cashAccountCode.id,
+                name: "cash account",
                 balance: 1_000_000_000_0000
             },
             {
-                id: EXPENSE_ACCOUNT_ID,
-                code: EXPENSE_ACCOUNT_CODE,
-                name: "expense",
-                type: "expense",
-                balance: 0,
-            },
-            {
-                id: BUDGET_PERALATAN_ID,
-                code: BUDGET_ACCOUNT_CODE,
-                name: "Budget Peralatan",
-                type: "asset",
-                balance: 0,
-            },
-            {
-                id: BUDGET_MAKAN_ID,
-                code: BUDGET_ACCOUNT_CODE,
-                name: "Budget Makan",
-                type: "asset",
-                balance: 0,
-            },
-            {
-                id: BUDGET_TRANSPORTASI_ID,
-                code: BUDGET_ACCOUNT_CODE,
-                name: "Budget Transportasi",
-                type: "asset",
+                codeId: expenseAccountCode.id,
+                name: "expense account",
                 balance: 0,
             },
         ],
     });
-
-    await entry(prisma, CASH_ACCOUNT_ID, BUDGET_PERALATAN_ID, BigInt(2_000_000_0000), "tambah saldo budget peralatan");
-    await entry(prisma, CASH_ACCOUNT_ID, BUDGET_MAKAN_ID, BigInt(2_000_000_0000), "tambah saldo budget makan");
-    await entry(prisma, CASH_ACCOUNT_ID, BUDGET_TRANSPORTASI_ID, BigInt(1_500_000_0000), "tambah saldo budget transportasi");
-    await entry(prisma, BUDGET_PERALATAN_ID, EXPENSE_ACCOUNT_ID, BigInt(300_000_0000), "beli sapu");
-    await entry(prisma, BUDGET_PERALATAN_ID, EXPENSE_ACCOUNT_ID, BigInt(200_000_0000), "beli pengki");
-    await entry(prisma, BUDGET_MAKAN_ID, EXPENSE_ACCOUNT_ID, BigInt(300_000_0000), "beli minum");
-    await entry(prisma, BUDGET_MAKAN_ID, EXPENSE_ACCOUNT_ID, BigInt(300_000_0000), "beli makan pagi");
-    await entry(prisma, BUDGET_MAKAN_ID, EXPENSE_ACCOUNT_ID, BigInt(300_000_0000), "beli makan malam");
-    await entry(prisma, BUDGET_MAKAN_ID, EXPENSE_ACCOUNT_ID, BigInt(300_000_0000), "beli makan sore");
-    await entry(prisma, BUDGET_TRANSPORTASI_ID, EXPENSE_ACCOUNT_ID, BigInt(60_000_0000), "naik gojek berangkat");
-    await entry(prisma, BUDGET_TRANSPORTASI_ID, EXPENSE_ACCOUNT_ID, BigInt(60_000_0000), "naik gojek pulang");
+    const expenseAccount = await prisma.account.findFirst({
+        where: {
+            codeId: expenseAccountCode.id,
+        }
+    });
+    const budgetPeralatan = await createBudget(prisma, "budget peralatan", BigInt(2_000_000_0000));
+    const budgetMakan = await createBudget(prisma, "budget makan", BigInt(2_000_000_0000));
+    const budgetTransportasi = await createBudget(prisma, "budget tranportasi", BigInt(1_500_000_0000));
+    await entry(prisma, budgetPeralatan.id, expenseAccount.id, BigInt(300_000_0000), "beli sapu");
+    await entry(prisma, budgetPeralatan.id, expenseAccount.id, BigInt(200_000_0000), "beli pengki");
+    await entry(prisma, budgetMakan.id, expenseAccount.id, BigInt(300_000_0000), "beli minum");
+    await entry(prisma, budgetMakan.id, expenseAccount.id, BigInt(300_000_0000), "beli makan pagi");
+    await entry(prisma, budgetMakan.id, expenseAccount.id, BigInt(300_000_0000), "beli makan malam");
+    await entry(prisma, budgetMakan.id, expenseAccount.id, BigInt(300_000_0000), "beli makan sore");
+    await entry(prisma, budgetTransportasi.id, expenseAccount.id, BigInt(60_000_0000), "naik gojek berangkat");
+    await entry(prisma, budgetTransportasi.id, expenseAccount.id, BigInt(60_000_0000), "naik gojek pulang");
 }
 
 main()
