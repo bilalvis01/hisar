6386085a926fd3756e888a4f210a527668b48e90
Bilal <bilalbi1992@gmail.com>
2024-04-26 09:38:50 +0700

    add component snackbar

---
+++ lib/components/Snackbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import clsx from "clsx";
+
+export function Snackbar({ children, className }: { children: React.ReactNode, className?: string }) {
+    return (
+        <div className={clsx("snackbar", className)}>
+            <div className="container">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export function Action({ children, ...props }: React.HTMLProps<HTMLButtonElement>) {
+    return (
+        <button {...props} type="button" className="action">
+            <div className="label">
+                {children}
+            </div>
+        </button>
+    );
+}
+
+export function IconAction({ children, ...props }: React.HTMLProps<HTMLButtonElement>) {
+    return (
+        <button {...props} type="button" className="icon-action">
+            <div className="icon">
+                {children}
+            </div>
+        </button>
+    );
+}
+
+export function SupportingText({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="supporting-text">
+            {children}
+        </div>
+    );
+}
