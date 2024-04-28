6599f42982f8a8a8e5964b3900e4d7aff8f8c548
Bilal <bilalbi1992@gmail.com>
2024-04-29 00:19:15 +0700

    add graphql document ADD_EXPENSE

--- lib/graphql-documents.ts
+++ lib/graphql-documents.ts
@@ -3,10 +3,13 @@ import { gql } from "./graphql-tag";
 export const GET_BUDGETS = gql(/* GraphQL */ `
     query GetBudget {
         budgets {
+            id
             name
             budget
             expense
             balance
+            createdAt,
+            updatedAt,
         }
     }
 `);
@@ -26,6 +29,36 @@ export const CREATE_BUDGET = gql(/* GraphQL */`
                 createdAt
                 updatedAt
             }
+        }
+    }
+`);
+
+export const ADD_EXPENSE = gql(/* GraphQL */`
+    mutation AddExpense($input: AddExpenseInput!) {
+        addExpense(input: $input) {
+            code
+            success
+            message
+            expense {
+                id
+                budgetAccount
+                amount
+                description
+                createdAt
+                updatedAt
+            }
+        }
+    }
+`);
+
+export const GET_EXPENSES = gql(/* GraphQL */ `
+    query GetExpenses {
+        expenses {
+            id
+            description
+            budgetAccount
+            budgetAccountId
+            amount
         }
     }
 `);
