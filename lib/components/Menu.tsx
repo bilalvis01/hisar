2e2155fd74cb63ea8e2a828227f62f5aae91059a
Bilal <bilalbi1992@gmail.com>
2024-04-28 12:26:03 +0700

    refactor component Menu

--- lib/components/Menu.tsx
+++ lib/components/Menu.tsx
@@ -1,31 +1,87 @@
 import React from "react";
 import clsx from "clsx";
 
-export function Menu({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="menu">
-            <div className="container">
-                {children}
-            </div>
-        </div>
-    );
+interface MenuContext {
+    value: string;
+    setValue: React.Dispatch<React.SetStateAction<string>>,
+    handleChange?: (value: string) => void;
 }
+
+const MenuContext = React.createContext<MenuContext>(null);
+
+const useMenuContext = () => {
+    const context = React.useContext(MenuContext);
+
+    if (context == null) {
+        throw new Error("<MenuItem /> must be wrapped in <Menu />");
+    }
+
+    return context;
+}
+
+type MenuProps = {
+    initialValue: string;
+    value: string;
+    onChange?: (value: string) => void | React.Dispatch<React.SetStateAction<string>>;
+} & React.HTMLProps<HTMLDivElement>;
+
+export const Menu = React.forwardRef<
+    HTMLDivElement,
+    MenuProps
+>(function Menu({ 
+    children, 
+    className, 
+    initialValue, 
+    value: controlledValue,
+    onChange: handleChange, 
+    ...props
+}, ref) {
+    const [uncontrolledValue, setValue] = React.useState(initialValue);
+    const value = controlledValue ?? uncontrolledValue;
+
+    const context = React.useMemo(() => ({
+        value,
+        setValue,
+        handleChange,
+    }), [value, handleChange]);
+
+    return (
+        <MenuContext.Provider value={context}>
+            <div {...props} ref={ref} className={clsx("menu", className)}>
+                <div className="container">
+                    {children}
+                </div>
+            </div>
+        </MenuContext.Provider>
+    );
+})
 
 interface MenuItemProps {
     children: React.ReactNode,
     startIcon?: React.ReactNode,
     endIcon?: React.ReactNode,
-    select?: boolean,
+    value: string,
 }
 
 export function MenuItem({ 
     children, 
     startIcon, 
     endIcon,
-    select = false,
+    value: thisValue,
 }: MenuItemProps) {
+    const { value, setValue, handleChange } = useMenuContext();
+
+    const handleClick = () => {
+        setValue(thisValue);
+        handleChange(thisValue);
+    }
+
     return (
-        <button type="button" className={clsx("list-item", { select })}>
+        <button 
+            type="button" 
+            className={clsx("list-item", { select: value === thisValue })}
+            onClick={handleClick}
+        >
             <div className="decorator">
                 <div className="state-layer" />
             </div>
