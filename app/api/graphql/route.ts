a6e615b4e9a0d9f2ebd6d1c3a45ffc6b2f8911cd
Bilal <bilalbi1992@gmail.com>
2024-05-14 05:10:17 +0700

    add resolvers query budgetByCode and DateTimeISO and typeDefs DateTimeISO

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -2,37 +2,99 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { Resolvers } from "../../../lib/resolvers-types";
 import { readFileSync } from 'fs';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Account, AccountCode } from "@prisma/client";
 import { createBudget, entry } from "../../../lib/transactions";
+import { DateTimeISOTypeDefinition, DateTimeISOResolver } from "graphql-scalars";
 
-async function fetchBugdets(client: PrismaClient) {
-    const data = await client.account.findMany({ 
-        where: { accountCode: { accountSupercode: { code: 101 } } },
-        include: {
-            entries: true,
+async function getBudgetDetail(
+    dataSources: PrismaClient, 
+    account: Account & { accountCode: AccountCode & { accountSupercode: AccountCode } }
+) {
+    const ledger = await dataSources.ledger.findMany({
+        where: {
+            entries: { some: { account: { id: account.id } } },
+            state: { id: 1 },
+        },
+        include: { 
+            entries: { 
+                include: { 
+                    account: true 
+                } 
+            } 
         },
         orderBy: {
             createdAt: "desc",
         },
     });
 
-    return data.map((record) => {
-        const budget = record.entries
-            .filter(val => val.direction == 1)
-            .reduce((acc, val) => acc + val.amount, BigInt(0));
-        const expense = record.entries
-            .filter(val => val.direction == -1)
-            .reduce((acc, val) => acc + val.amount, BigInt(0));
-        return {
-            id: record.id,
-            name: record.name,
-            budget: Number(budget / BigInt(10000)),
-            expense: Number(expense / BigInt(10000)),
-            balance: Number(record.balance / BigInt(10000)),
-            createdAt: record.createdAt.toISOString(),
-            updatedAt: record.updatedAt.toISOString(),
+    const budget = ledger.reduce((acc, val) => {
+        const entry = val.entries.filter((entry) => entry.direction === 1 && entry.account.id === account.id)[0];
+        if (entry) {
+            return acc + entry.amount;
         }
+        return acc;
+    }, BigInt(0));
+    
+    const expense = ledger.reduce((acc, val) => {
+        const entry = val.entries.filter((entry) => entry.direction === -1 && entry.account.id === account.id)[0];
+        if (entry) {
+            return acc + entry.amount;
+        }
+        return acc;
+    }, BigInt(0));
+    
+    const expenseDetail = ledger.map((record) => record.entries.reduce((acc, entry) => {
+        if (entry.account.id === account.id) {
+            if (entry.direction === 1) {
+                return {
+                    description: record.description,
+                    debit: Number(entry.amount / BigInt(10000)),
+                    balance: Number(entry.balance / BigInt(10000)),
+                }
+            } else {
+                return {
+                    description: record.description,
+                    credit: Number(entry.amount / BigInt(10000)),
+                    balance: Number(entry.balance / BigInt(10000)),
+                }
+            }
+        }
+
+        return acc;
+    }, {
+        description: "",
+        balance: 0,
+    }));
+
+    return {
+        id: account.id,
+        code: `${account.accountCode.accountSupercode.code}-${account.accountCode.code}`,
+        name: account.name,
+        budget: Number(budget / BigInt(10000)),
+        expense: Number(expense / BigInt(10000)),
+        balance: Number(account.balance / BigInt(10000)),
+        expenseDetail,
+        createdAt: account.createdAt,
+        updatedAt: account.updatedAt,
+    }
+}
+
+async function fetchBugdets(dataSources: PrismaClient) {
+    const data = await dataSources.account.findMany({ 
+        where: { accountCode: { accountSupercode: { code: 101 } } },
+        include: {
+            accountCode: {
+                include: {
+                    accountSupercode: true
+                }
+            }
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
     });
+
+    return await Promise.all(data.map(async (account) => await getBudgetDetail(dataSources, account)));
 };
 
 const resolvers: Resolvers = {
@@ -57,6 +119,27 @@ const resolvers: Resolvers = {
             return await fetchBugdets(context.dataSources);
         },
 
+        async budgetByCode(_, { code: code_ }, context) {
+            const code = code_.split("-").map(Number);
+            const account = await context.dataSources.account.findFirst({
+                where: {
+                    accountCode: { 
+                        code: code[1], 
+                        accountSupercode: { code: code[0] },
+                    },
+                },
+                include: {
+                    accountCode: {
+                        include: {
+                            accountSupercode: true
+                        }
+                    }
+                }
+            });
+
+            return await getBudgetDetail(context.dataSources, account);
+        },
+
         async expenses(_, __, context) {
             const data = await context.dataSources.ledger.findMany({ 
                 where: { entries: { some: { account: { accountCode: { code: 200 } } } } },
@@ -79,8 +162,8 @@ const resolvers: Resolvers = {
                     budgetAccount: budgetAccount.name,
                     budgetAccountId: budgetAccount.id,
                     amount: Number(expenseEntry.amount / BigInt(10000)),
-                    createdAt: record.createdAt.toISOString(),
-                    updatedAt: record.updatedAt.toISOString(),
+                    createdAt: record.createdAt,
+                    updatedAt: record.updatedAt,
                 };
             });
         }
@@ -100,12 +183,14 @@ const resolvers: Resolvers = {
                     message: `akun ${input.name} berhasil dibuat`,
                     budget: {
                         id: account.id,
+                        code: `${account.accountCode.code}-${account.accountCode.accountSupercode.code}`,
                         name: account.name,
                         budget: Number(account.balance),
                         expense: 0,
+                        expenseDetail: [],
                         balance: Number(account.balance),
-                        createdAt: account.createdAt.toISOString(),
-                        updatedAt: account.updatedAt.toISOString(),
+                        createdAt: account.createdAt,
+                        updatedAt: account.updatedAt,
                     },
                 };
             } catch (err) {
@@ -143,8 +228,8 @@ const resolvers: Resolvers = {
                         budgetAccount: budgetAccount.name,
                         amount: input.amount,
                         description: ledger.description,
-                        createdAt: ledger.createdAt.toISOString(),
-                        updatedAt: ledger.updatedAt.toISOString(),
+                        createdAt: ledger.createdAt,
+                        updatedAt: ledger.updatedAt,
                     },
                 };
             } catch (error) {
@@ -161,7 +246,16 @@ const resolvers: Resolvers = {
 
 const typeDefs = readFileSync(process.cwd() + '/schema.graphql', 'utf8');
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({ 
+    resolvers: {
+        DateTimeISO: DateTimeISOResolver,
+        ...resolvers
+    }, 
+    typeDefs: [
+        DateTimeISOTypeDefinition,
+        typeDefs,
+    ],
+});
 
 const handler = startServerAndCreateNextHandler(server, {
     context: async () => ({
