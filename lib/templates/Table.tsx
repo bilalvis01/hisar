2cfe19e93e921f7cbb30f2c0a776f1add4dd19e0
Bilal <bilalbi1992@gmail.com>
2024-05-14 05:20:41 +0700

    refactor template component Table

--- lib/templates/Table.tsx
+++ lib/templates/Table.tsx
@@ -3,20 +3,12 @@
 import React from "react";
 import { flexRender } from "@tanstack/react-table";
 import type { Table } from "@tanstack/react-table";
-import ProgressCircular from "../../components/ProgressCircular";
-import style from "./Table.module.scss";
 
 interface TableProps<Column> {
-    loading: boolean;
-    success: boolean;
-    message: string;
     table: Table<Column>;
 };
 
-export default function Table<Column>({ loading, success, message, table }: TableProps<Column>) {
-    if (loading) return <div className={style.placeholder}><ProgressCircular /></div>;
-    if (!success) return <div className={style.placeholder}>{message}</div>;
-
+export default function Table<Column>({ table }: TableProps<Column>) {
     return (
         <>
             <table className="table">
