f3c8305a1540c9cc03b2db8904444b18fefd0e8f
Bilal <bilalbi1992@gmail.com>
2024-05-23 19:18:33 +0700

    refactor database seed script

--- seed.dev.ts
+++ seed.dev.ts
@@ -1,138 +1,165 @@
 import { PrismaClient } from '@prisma/client';
-import { createBudget, createExpense } from "./lib/database/transactions";
+import { 
+    createBudgetProcedure, 
+    createExpenseProcedure, 
+    journalizeProcedure,
+} from "./lib/database/procedures";
 import { 
     CASH_ACCOUNT_CODE, 
     EXPENSE_ACCOUNT_CODE, 
-    BUDGET_ACCOUNT_CODE 
+    BUDGET_ACCOUNT_CODE,
+    PRIVE_ACCOUNT_CODE, 
 } from './lib/database/account-code';
-import { 
-    ACTIVE,
-    HIDE,
-    ACCOUNT_SOFT_DELETED,
-    LEDGER_SOFT_DELETED,
-} from './lib/database/state';
+import {
+    ASSET,
+    EXPENSE,
+    PRIVE,
+} from "./lib/database/account-type";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.state.createMany({
-        data: [
-            {
-                id: ACTIVE,
-                name: "active",
+    await prisma.$transaction(async (tx: PrismaClient) => {
+        const assetAccountType = await tx.accountType.create({
+            data: {
+                id: ASSET,
+                name: "asset",
+                direction: 1,
             },
-            {
-                id: HIDE,
-                name: "hide",
+        });
+    
+        const expenseAccountType = await tx.accountType.create({
+            data: {
+                id: EXPENSE,
+                name: "expense",
+                direction: -1,
             },
-            {
-                id: ACCOUNT_SOFT_DELETED,
-                name: "account_soft_deleted",
+        });
+    
+        const priveAccountType = await tx.accountType.create({
+            data: {
+                id: PRIVE,
+                name: "prive",
+                direction: -1,
             },
-            { 
-                id: LEDGER_SOFT_DELETED,
-                name: "ledger_soft_deleted",
-            },
-        ],
-    });
-    await prisma.accountCode.createMany({
-        data: [
-            {
+        });
+    
+        const cashAccountCode = await tx.accountCode.create({
+            data: {
                 code: CASH_ACCOUNT_CODE,
-                accountType: "assets",
-            },
-            {
-                code: EXPENSE_ACCOUNT_CODE,
-                accountType: "expenses",
-            },
-            {
-                code: BUDGET_ACCOUNT_CODE,
-                accountType: "assets",
             }
-        ]
-    });
-    const cashAccountCode = await prisma.accountCode.findFirst({
-        where: {
-            code: CASH_ACCOUNT_CODE,
-        }
-    });
-    const expenseAccountCode = await prisma.accountCode.findFirst({
-        where: {
-            code: EXPENSE_ACCOUNT_CODE,
-        }
-    });
-    await prisma.account.createMany({
-        data: [
-            {
-                accountCodeId: cashAccountCode.id,
+        });
+    
+        const expenseAccountCode = await tx.accountCode.create({
+            data: {
+                code: EXPENSE_ACCOUNT_CODE,
+            }
+        });
+    
+        const budgetAccountCode = await tx.accountCode.create({
+            data: {
+                code: BUDGET_ACCOUNT_CODE,
+            }
+        });
+    
+        const priveAccountCode = await tx.accountCode.create({
+            data: {
+                code: PRIVE_ACCOUNT_CODE,
+            }
+        });
+    
+        const cashAccount = await tx.account.create({
+            data: {
                 name: "cash account",
-                balance: 1_000_000_000_0000,
-                direction: 1,
+                accountCode: { connect: { id: cashAccountCode.id } },
+                accountType: { connect: { id: assetAccountType.id } },
             },
-            {
-                accountCodeId: expenseAccountCode.id,
-                name: "expense account",
-                balance: 0,
-                direction: 1,
+        });
+    
+        const priveAccount = await tx.account.create({
+            data: {
+                name: "prive account",
+                accountCode: { connect: { id: priveAccountCode.id } },
+                accountType: { connect: { id: priveAccountType.id } },
             },
-        ],
-    });
-    const expenseAccount = await prisma.account.findFirst({
-        where: {
-            accountCodeId: expenseAccountCode.id,
-        }
-    });
-    const budgetPeralatan = await createBudget(prisma, { 
-        name: "budget peralatan", 
-        amount: BigInt(2_000_000_0000),
-    });
-    const budgetMakan = await createBudget(prisma, {
-        name: "budget makan", 
-        amount: BigInt(2_000_000_0000),
-    });
-    const budgetTransportasi = await createBudget(prisma, {
-        name: "budget tranportasi", 
-        amount: BigInt(1_500_000_0000),
-    });
-    await createExpense(prisma, { 
-        budgetAccountId: budgetPeralatan.id, 
-        amount: BigInt(300_000_0000), 
-        description: "beli sapu",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetPeralatan.id, 
-        amount: BigInt(200_000_0000), 
-        description: "beli pengki", 
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetMakan.id, 
-        amount: BigInt(300_000_0000), 
-        description: "beli minum",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetMakan.id,
-        amount: BigInt(300_000_0000), 
-        description: "beli makan pagi",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetMakan.id, 
-        amount: BigInt(300_000_0000), 
-        description: "beli makan malam",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetMakan.id, 
-        amount: BigInt(300_000_0000), 
-        description: "beli makan sore",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetTransportasi.id,
-        amount: BigInt(60_000_0000), 
-        description: "naik gojek berangkat",
-    });
-    await createExpense(prisma, {
-        budgetAccountId: budgetTransportasi.id,
-        amount: BigInt(60_000_0000), 
-        description: "naik gojek pulang",
+        });
+    
+        const cashAccountLedger = await tx.ledger.create({
+            data: {
+                balance: BigInt(0),
+                account: { connect: { id: cashAccount.id, } },
+            },
+        });
+    
+        const priveAccountLedger = await tx.ledger.create({
+            data: {
+                balance: BigInt(0),
+                account: { connect: { id: priveAccount.id, } },
+            },
+        });
+    
+        await journalizeProcedure(tx, {
+            debitAccountId: cashAccount.id,
+            creditAccountId: priveAccount.id,
+            amount: BigInt(1_000_000_000_0000),
+            description: "modal awal",
+        });
+    
+        const budgetPeralatan = await createBudgetProcedure(tx, { 
+            name: "budget peralatan", 
+            amount: BigInt(2_000_000_0000),
+        });
+    
+        const budgetMakan = await createBudgetProcedure(tx, {
+            name: "budget makan", 
+            amount: BigInt(2_000_000_0000),
+        });
+    
+        const budgetTransportasi = await createBudgetProcedure(tx, {
+            name: "budget tranportasi", 
+            amount: BigInt(1_500_000_0000),
+        });
+    
+        await createExpenseProcedure(tx, { 
+            budgetId: budgetPeralatan.id, 
+            amount: BigInt(300_000_0000), 
+            description: "beli sapu",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetPeralatan.id, 
+            amount: BigInt(200_000_0000), 
+            description: "beli pengki", 
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetMakan.id, 
+            amount: BigInt(300_000_0000), 
+            description: "beli minum",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetMakan.id,
+            amount: BigInt(300_000_0000), 
+            description: "beli makan pagi",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetMakan.id, 
+            amount: BigInt(300_000_0000), 
+            description: "beli makan malam",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetMakan.id, 
+            amount: BigInt(300_000_0000), 
+            description: "beli makan sore",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetTransportasi.id,
+            amount: BigInt(60_000_0000), 
+            description: "naik gojek berangkat",
+        });
+        await createExpenseProcedure(tx, {
+            budgetId: budgetTransportasi.id,
+            amount: BigInt(60_000_0000), 
+            description: "naik gojek pulang",
+        });
     });
 }
 
