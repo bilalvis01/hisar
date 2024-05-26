5a41d8564f1fbae6e09efbae8f8da3836ac5782e
Bilal <bilalbi1992@gmail.com>
2024-05-26 19:13:44 +0700

    add fields createdAt and updatedAt to model Entry

--- prisma/migrations/20240526121127_init/migration.sql
+++ prisma/migrations/20240526121127_init/migration.sql
@@ -68,6 +68,8 @@ CREATE TABLE `Entry` (
     `softDeleted` BOOLEAN NOT NULL DEFAULT false,
     `journalId` INTEGER NOT NULL,
     `ledgerId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
