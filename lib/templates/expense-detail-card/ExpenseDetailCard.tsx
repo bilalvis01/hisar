5b3eed3b8e3405267bed649780a9a54fac05ed7a
Bilal <bilalbi1992@gmail.com>
2024-05-27 21:13:16 +0700

    tweak

--- lib/templates/expense-detail-card/ExpenseDetailCard.tsx
+++ lib/templates/expense-detail-card/ExpenseDetailCard.tsx
@@ -118,7 +118,6 @@ export default function ExpenseDetailCard() {
                         expense={expense}
                         open={openExpenseUpdateForm}
                         onOpenChange={setOpenExpenseUpdateForm}
-                        disableBudgetSelection={true}
                     />
                 )}
                 {expense && (
