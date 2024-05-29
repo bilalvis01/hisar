2b26292ef553110d5811bf5ad13553e551cd3e17
Bilal <bilalbi1992@gmail.com>
2024-05-29 12:22:53 +0700

    made the selection table's column sticky

--- lib/templates/table/Table.tsx
+++ lib/templates/table/Table.tsx
@@ -17,28 +17,40 @@ export default function Table<Column>({ table, className }: TableProps<Column>) 
                 <thead>
                     {table.getHeaderGroups().map(headerGroup => (
                         <tr key={headerGroup.id}>
-                            {headerGroup.headers.map(header => (
-                                <th key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )
-                                    }
-                                </th>
-                            ))}
+                            {headerGroup.headers.map(header => {
+                                let className = header.column.id === "select"
+                                    ? "selection"
+                                    : null;
+
+                                return (
+                                    <th key={header.id} className={className}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )
+                                        }
+                                    </th>
+                                )
+                            })}
                         </tr>
                     ))}
                 </thead>
                 <tbody>
                     {table.getRowModel().rows.map(row => (
                         <tr key={row.id}>
-                            {row.getVisibleCells().map(cell => (
-                                <td key={cell.id}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </td>
-                            ))}
+                            {row.getVisibleCells().map(cell => {
+                                let className = cell.column.id === "select"
+                                    ? "selection"
+                                    : null;
+
+                                return (
+                                    <td key={cell.id} className={className}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                );
+                            })}
                         </tr>
                     ))}
                 </tbody>
