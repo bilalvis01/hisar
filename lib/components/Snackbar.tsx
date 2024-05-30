0dfb04e1346b1ef679f1d73e7eaf2ac2f06dce04
Bilal <bilalbi1992@gmail.com>
2024-05-30 15:52:31 +0700

    fix snackbar

--- lib/components/Snackbar.tsx
+++ lib/components/Snackbar.tsx
@@ -1,13 +1,46 @@
 import React from "react";
 import clsx from "clsx";
 
+interface SnackbarContext {
+    twoLine: boolean;
+    setTwoLine: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SnackbarContext = React.createContext<SnackbarContext>(null);
+
+function useSnackbarContext() {
+    const context = React.useContext(SnackbarContext);
+
+    if (context == null) {
+        throw new Error("Snackbar components must be wrapped in <Snackbar />");
+    }
+
+    return context;
+}
+
+function useSnackbar() {
+    const [twoLine, setTwoLine] = React.useState(false);
+
+    return React.useMemo(() => ({
+        twoLine,
+        setTwoLine,
+    }), [twoLine]);
+}
+
 export function Snackbar({ children, className, ...props }: React.HTMLProps<HTMLDivElement>) {
+    const context = useSnackbar();
+
     return (
-        <div {...props} className={clsx("snackbar", className)}>
-            <div className="container">
-                {children}
+        <SnackbarContext.Provider value={context}>
+            <div 
+                {...props} 
+                className={clsx("snackbar", { "two-line": context.twoLine }, className)}
+            >
+                <div className="container">
+                    {children}
+                </div>
             </div>
-        </div>
+        </SnackbarContext.Provider>
     );
 }
 
@@ -32,8 +65,23 @@ export function IconAction({ children, ...props }: React.HTMLProps<HTMLButtonEle
 }
 
 export function SupportingText({ children }: { children: React.ReactNode }) {
+    const { setTwoLine, twoLine } = useSnackbarContext();
+    const ref = React.useRef(null);
+
+    React.useEffect(() => {
+        if (ref.current instanceof HTMLDivElement) {
+            if (ref.current.clientWidth < ref.current.scrollWidth) {
+                setTwoLine(true);
+            }
+        }
+    });
+
     return (
-        <div className="supporting-text">
+        <div 
+            ref={ref} 
+            className="supporting-text"
+            style={{ whiteSpace: twoLine ? "wrap" : "nowrap"  }}
+        >
             {children}
         </div>
     );
