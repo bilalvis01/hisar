934d8fb435a5b2a62763c0e4ef2981656da68f2b
Bilal <bilalbi1992@gmail.com>
2024-06-01 20:20:13 +0700

    prevent creating new instances of PrismaClienat on hot reloading on dev enviroment

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -345,6 +345,12 @@ const server = new ApolloServer({
     ],
 });
 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
 const handler = startServerAndCreateNextHandler(server, {
     context: async () => ({
         dataSources: new PrismaClient()
